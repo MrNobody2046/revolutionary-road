a5d53bf1d428615ee9398fac626e78924c3c630e
philoprove <sphy@foxmail.com>
2014-09-18 17:07:23 +0800

    training "gift1" with python.

---
+++ usaco/ch1/gift1.py
@@ -0,0 +1,40 @@
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+        self.friends = []
+        self.get = 0.0
+        self.give = 0.0
+
+    def __str__(self):
+        return "Name:%9s,Get:%d,Paied:%d,Revenue:%d" % (self.name, self.get, self.give, self.get - self.give)
+
+
+persons = {}
+with open("gift1.in") as fin:
+    def readline():
+        return fin.readline().strip()
+    index = 0
+    NP = int(readline())
+    while NP != 0:
+        name = readline()
+        persons[name] = Person(name)
+        NP -= 1
+    while True:
+        host = readline()
+        if not host:
+            break
+        pay, friend_count = tuple([int(i) for i in readline().split()])
+        host = persons[host]
+        if friend_count > 0:
+            average_pay = round(pay / friend_count)
+            host.give = average_pay * friend_count
+            while friend_count != 0:
+                friend_count -= 1
+                friend = persons[readline()]
+                friend.get += average_pay
+                host.friends.append(friend)
+
+for p in sorted(persons.values()):
+    print p
+
