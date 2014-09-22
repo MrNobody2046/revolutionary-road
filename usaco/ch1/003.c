f5b62463d0a22124e208c99248ab334ef771a1b7
kenny <kenny@kenny@xmouse.vm.com>
2014-09-22 23:09:03 +0800

    finished code

--- usaco/ch1/003.c
+++ usaco/ch1/003.c
@@ -1,7 +1,7 @@
 /*
 ID:sphy1
 LANG: C
-TASK: gift1
+TASK: friday
 */
 
 #include <stdio.h>
@@ -25,17 +25,68 @@ int isleapyear(int year){
         return 0;
 }
 
+int readoffset(){
+    int ret;
+    FILE *fin;
+    fin=fopen("friday.in","r");
+    fscanf(fin,"%d",&ret);
+    fclose(fin);
+    return ret;
+}
+
+
+int daycount(struct year y){
+    int i,count=0;
+    for(i=0;i<12;i++){
+        count += y.daynum[i];
+    }
+    return count;
+}
+
+void count13(struct year y,int res[]){
+    int i,month_lastday;
+    month_lastday = (y.fisrtday + 6) % 7;
+    for(i=0;i<12;i++){
+        printf("%d,",(month_lastday + 13) % 7);
+        res[(month_lastday + 13) % 7]++;
+        month_lastday = (month_lastday + y.daynum[i]) % 7;
+    }
+    printf("\n");
+}
 
 int main(){
     int norm[] = {31,28,31,30,31,30,31,31,30,31,30,31};
     int leap[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+    int res[] = {0,0,0,0,0,0,0};
     int i;
-    struct year start;
-    start.daynum = norm;
-    start.fisrtday = 1;
-    for(i=0;i<12;i++){
-        printf("%d\n",*(start.daynum+i));
+    int offset=readoffset();
+    int lastday=6; // the week day of last year
+    for(i=0;i<offset;i++){
+        struct year temp;
+        if(isleapyear(START_YEAR+i)==1){
+            printf("is leap %d\n",START_YEAR+i);
+            temp.daynum = leap;
+        }
+        else
+            temp.daynum = norm;
+        temp.fisrtday = (lastday+1) % 7;
+        printf("%d\n",lastday);
+        lastday = (lastday + daycount(temp)) % 7;
+        count13(temp,res);
+
     }
-    //printf("Isleap:%d",isleapyear(2004));
+
+    FILE *fout;
+    char count[10];
+    fout = fopen("friday.out","w");
+    for(i=0;i<7;i++){
+        printf("%d,%d\n",i,res[i]);
+        sprintf(count,"%d",res[i]);
+        fputs(count,fout);
+        fputs(" ",fout);
+    }
+    fclose(fout);
+    exit(0);
+//    printf("Isleap:%d",readoffset());
 
 }
