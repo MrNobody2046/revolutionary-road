5f918921a45f108f6271b1b85ba5c51a5d2e9488
philoprove <sphy@foxmail.com>
2014-09-25 18:41:17 +0800

    ...

--- usaco/ch1/beads.py
+++ usaco/ch1/beads.py
@@ -1,7 +1,7 @@
 # coding:utf-8
-a = "rwrwrwrwrwrwrwrwrwrwrwrwbwrwbwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwr"
-
-l = len(a)
+a = "rrr"
+a = "brbrrrbbbrrrrrbrrbbrbbbbrrrrb"
+length = len(a)
 """
 最长串的条件：
 
@@ -9,15 +9,71 @@ l = len(a)
 中间只能变化一次
 """
 
+def count_distance(start, end, length):
+    if start > end:
+        return length - start + end + 1
+    else:
+        return end - start + 1
+trunk = a
+max_counts = 0
+mid = end = cursor = 0
+temp = 0
+color = ""
+start = end = length
+fstart = -1
+pre_start = -1
+sample = ""
+for i in range(13):
+    color = trunk[start % length]
+    print color
+    while temp < length:
+        temp += 1  # 循环计数器
+        start -= 1
+        sample = trunk[start % length]
+        if sample != color and sample != "w":
+            if color == "w":
+                color = sample
+            else:
+                break
+    start = (start+1) % length
+    while temp < length:
+        temp += 1
+        end += 1
+        sample = trunk[end % length]
+        if sample != color and sample != "w":
+            break
+    end = end % length
+    print start,end,temp
+    if temp>=length-1:
+        max_counts = length
+        break
+    count = temp - 1
+
+    if pre_start < 0:
+        pre_start = end
+    else:
+        if pre_start != start:
+            #修正偏差
+            count -= (pre_start - start) % length
+        pre_start = end
+    if count >max_counts:
+        max_counts = count
+    start = end
+    if start >= fstart:
+        print end,fstart
+    temp = 0
+        # break
+
+print "MAx len",max_counts
 
 def spliting(li, index=0):
     # back serach: 结果是闭合区间
     length = len(li)
     start = index + length
-    flag = li[start % length] # 决定这一段内容是r还是b 初始值可能是wrb中的任意一种
-    j = 0
-    while j < length:
-        j -= 1 # 循环计数器
+    flag = li[start % length]  # 决定这一段内容是r还是b 初始值可能是wrb中的任意一种
+    temp = 0
+    while temp < length:
+        temp += 1  # 循环计数器
         start -= 1
         back = li[start % length]
         if back != flag and back != "w":
@@ -28,12 +84,13 @@ def spliting(li, index=0):
     start = start + 1
     if start > length:
         start = start % length
-    remain = (index + j + length) % length
+    remain = (index + temp + length) % length
     # print "Flag is:%s,Start is:%d，Remain:%d" % (flag,start,remain)
     end = index
     # print "SearchFront,Start from:%d" % index
-    while j < remain:
-        j += 1
+    temp = 0
+    while temp < remain:
+        temp += 1
         end += 1
         front = li[end % length]
         if front != flag and front != "w":
@@ -41,25 +98,52 @@ def spliting(li, index=0):
     end -= 1
     if end > length:
         end = end % length
-    print "Start:%d,End:%d" % (start,end)
+    print "Start:%d,End:%d" % (start, end)
     return start, end
+#
+#
 
-def count_distance(start,end,length):
-    if start > end:
-        return length - start + end + 1
-    else:
-        return end - start + 1
-
-start,end = spliting(a)
-fend = end
-count = []
-while True:
-    start,end = spliting(a,index=end+1)
-    distance = count_distance(start,end,29)
-    count.append(distance)
-    if fend - end == 0:
-        break
-print count
+#
+# spliting(a, index=1)
+# spliting(a, index=3)
+# end = 0
+# interval = []
+# span = 0
+# max_count = 0
+# fstart = None
+# while True:
+#     start, end = spliting(a, index=end+1)
+#     if not fstart:
+#         fstart = start
+#         print fstart
+#     if len(interval) == 0:
+#         interval.append((start, end ))
+#     elif len(interval) == 1:
+#         interval.append((start, end ))
+#     else:
+#         interval[0] = interval[1]
+#         interval[1] = (start, end )
+#     if len(interval) == 1:
+#         count = count_distance(interval[0][0], interval[0][1], l)
+#         if count > max_count:
+#             max_count = count
+#     if len(interval) == 2:
+#         count = count_distance(interval[0][0], interval[1][1], l)
+#         if count > max_count:
+#             max_count = count
+#     if end == fstart:
+#         break
+# print max_count
+# start,end = spliting(a)
+# fend = end
+# count = []
+# while True:
+# start,end = spliting(a,index=end+1)
+#     distance = count_distance(start,end,29)
+#     count.append(distance)
+#     if fend - end == 0:
+#         break
+# print count
 # start = 0
 # for i,v in enumerate(a):
 # _n = a[(i+1)%l]
