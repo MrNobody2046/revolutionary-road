3c4fd6019463647b84a4d5670d42333bab91948a
kenny <kenny@kenny@xmouse.vm.com>
2014-09-28 00:01:11 +0800

    passed

--- usaco/ch1/s2/milk2.c
+++ usaco/ch1/s2/milk2.c
@@ -21,7 +21,7 @@ int read_lines(char data[][MAX_LINE_LENGTH]){
         if(*(data[i] + strlen(data[i]) - 1)=='\n'){
             *(data[i] + strlen(data[i]) - 1) = 0; //remove \n
             }
-        printf("%s\n",data[i]);
+        printf("readline:\"%s\"\n",data[i]);
         i ++;
 
     }
@@ -36,51 +36,61 @@ int compare(int target[],int sample[]){
     return sample[0] - target[0];
 }
 
+void copy(int from[],int to[],int length){
+    for(length --;length>=0;length--){
+        to[length] = from[length];
+    }
+}
+
 void bisect(int data[],int trunk[][2],int length,int (*compare)(int [],int [])){
     // length is items contains in trunk, not the trunk size.
     int high = length, low=0;
-    int mid = (high + low) / 2;
+    int mid = high / 2;
     int cond;
     while(low < high){
-        cond = compare(trunk[mid], data);
+        cond = compare(trunk[mid],data);
+        printf("%d  %d ;%d\n",trunk[mid][0],data[0],cond);
         if(cond > 0)
             low = mid + 1;
-        else if(cond < 0)
-            high = mid;
         else
-            break;
-        mid = (high + mid) / 2;
+            high = mid;
+        mid = low + (high-low) / 2;
     }
-    trunk[mid][0] = data[0];
-    trunk[mid][1] = data[1];
+    printf("insert into[%d]\t%d:%d\n",mid,data[0],data[1]);
+    if(mid<length){
+        int i=length;
+        for(;i>mid;i--){
+            copy(trunk[i-1],trunk[i],2);
+        }
+    }
+    copy(data,trunk[mid],2);
+
 }
 
 void read_points(int ret[2],char c[]){
-    int l=strlen(c),start=-2,i;
-    char b[l];
-    for(i=0;i<l;i++){
-        printf("%c",c[i]);
-        if(start==-1)
-            start=0;
-        if(c[i]==' ')
-            start ++;
-            c[i] = '\0';
-        if(start>=0)
-            b[start] = c[i];
+    int l = strlen(c);
+    int i, j, k;
+    char temp[l];
+    i = j = k = 0;
+    for(;i<=l;i++){
+        if(c[i]!=' '&&c[i]!='\0'){
+            temp[k] = c[i];
+            k ++;
+        }
+        else{
+            temp[k++] = '\n';
+            ret[j] = atoi(temp);
+            j ++;
+            k = 0;
+        }
     }
-    ret[0] = atoi(c);
-    ret[1] = atoi(b);
-    printf("from:%sto%s\n",c,b);
 }
     
-
-
 int main(){
     char data[MAX_FARMER_NUM][MAX_LINE_LENGTH];
     int farmer_num;
     read_lines(data);
     farmer_num = atoi(data[0]);
-    printf("%d",farmer_num);
     int i;
     int data_points[farmer_num][2];
     int temp[2];
@@ -89,5 +99,53 @@ int main(){
         read_points(temp,data[i+1]);
         bisect(temp, data_points, i, comp);
     }
+    int sorted[farmer_num][2],j;
+    for(i=0,j=0;i<farmer_num;i++){
+        //printf("work from:%d to %d\n",data_points[i][0],data_points[i][1]);
+        if(i!=0){
+            if(data_points[i][0] > sorted[j-1][1])
+                copy(data_points[i],sorted[j],2),j++;
+            else if(data_points[i][1]>sorted[j-1][1])
+                    sorted[j-1][1] = data_points[i][1];
+        }
+        else{
+            copy(data_points[i],sorted[i],2),j++;
+        }
+    }
+    int work_time;
+    int max_work_time=0;
+    int idle[farmer_num],k=0;
+    for(i=0;i<j;i++){
+        //printf("handle from:%d to %d\n",sorted[i][0],sorted[i][1]);
+        work_time = sorted[i][1] - sorted[i][0];
+        if(work_time > max_work_time)
+            max_work_time = work_time;
+        if(i==0)
+            idle[k] = sorted[i][1],k++;
+        else{
+            idle[k] = sorted[i][0],k++;
+            idle[k] = sorted[i][1],k++;
+        }
+    }
+    int idle_length = k;
+    i = 0;
+    int idle_time, max_idle_time;
+    while(1){
+        if(i>=k-1)
+            break;
+        idle_time = idle[i+1] - idle[i];
+        if(idle_time > max_idle_time)
+            max_idle_time = idle_time;
+        i += 2;
+    }
+    FILE *fout;
+    fout = fopen("milk2.out","w");
+    char buffer[MAX_LINE_LENGTH];
+    sprintf(buffer,"%d",max_work_time);
+    fputs(buffer,fout);
+    fputs(" ",fout);
+    sprintf(buffer,"%d",max_idle_time);
+    fputs(buffer,fout);
+    fputs("\n",fout);
     exit(0);
 }
