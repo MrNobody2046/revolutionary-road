50a00f1d71e58576ee3b1b203519c476642777bd
kenny <kenny@kenny@xmouse.vm.com>
2014-09-28 23:21:40 +0800

    plain way to solve it.

--- usaco/ch1/s2/transform.c
+++ usaco/ch1/s2/transform.c
@@ -8,6 +8,8 @@ TASK: transform
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 32
+
 void copy(int from[],int to[],int length){
     for(length --;length>=0;length--)
         to[length] = from[length];
@@ -41,21 +43,65 @@ void transform(int point[2],int ret[2],int size,int type){
 }
 
 void printp(int point[2]){
-    printf("<point:%2d ,%2d >\n",point[0],point[1]);
+    printf("<point:[%2d][%2d]>\n",point[0],point[1]);
 }
 
+
 int main(){
-    int p[]={1,1};
-    int ret[2];
-    transform(p,ret,5,0);
-    printp(ret);
-    transform(p,ret,5,1);
-    printp(ret);
-    transform(p,ret,5,2);
-    printp(ret);
-    transform(p,ret,5,3);
-    printp(ret);
-    transform(p,ret,5,4);
-    printp(ret);
+//    int p[]={1,1};
+//    int ret[2];
+//    transform(p,ret,5,0);
+//    printp(ret);
+//    transform(p,ret,5,1);
+//    printp(ret);
+//    transform(p,ret,5,2);
+//    printp(ret);
+//    transform(p,ret,5,3);
+//    printp(ret);
+//    transform(p,ret,5,4);
+//    printp(ret);
+
+    FILE *fin,fout;
+    fin = fopen("transform.in","r");
+    char temp[MAX_LINE_LENGTH];
+    fgets(temp,MAX_LINE_LENGTH,fin);
+    int size = atoi(temp),i,j,type;
+    printf("size:%d",size);
+    char orig[size][size];
+    for(i=0;i<size;i++){
+        fgets(temp,MAX_LINE_LENGTH,fin);
+        *(temp+size * sizeof(char)) = '\0';
+        strcpy(orig[i],temp);
+    }
+    char changed[size][size];
+    for(i=0;i<size;i++){
+        fgets(temp,MAX_LINE_LENGTH,fin);
+        *(temp+size * sizeof(char)) = '\0';
+        strcpy(changed[i],temp);
+    }
+    int op[2],tp[2],brk=0,passed=-1;
+    for(type=0;type<7;type++){
+        for(i=0;i<size;i++){
+            if(brk==1)
+                break;
+            for(j=0;j<size;j++){
+                op[0] = i,op[1] = j;
+                transform(op,tp,size,type);
+                printp(op),printp(tp);
+                printf("Compare[%c] with [%c]\n",orig[i][j],changed[tp[0]][tp[1]]);
+                if(orig[i][j] != changed[tp[0]][tp[1]]){
+                    brk = 1;
+                    printf("Break@type:%d",type);
+                    break; }
+                }
+            }
+        if(brk==1)
+            brk =0;
+        else{
+            passed =type;
+            break;
+        }
+    }
+    printf("Passed:%d",passed);
     exit(0);
 }
