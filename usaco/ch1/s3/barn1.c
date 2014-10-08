6628531ff5b28a994c52f576b8623a4bdde42e78
kenny <kenny@kenny@xmouse.vm.com>
2014-10-08 22:04:55 +0800

    ...

--- usaco/ch1/s3/barn1.c
+++ usaco/ch1/s3/barn1.c
@@ -12,10 +12,42 @@ TASK: barn1
 #define OUTPUT "barn1.out"
 
 
+void bisect(int num,int list[],int length){
+    int low, mid, high;
+    low = 0, mid = length / 2, high = length;
+    while(low < high){
+        if((list[mid] - num) > 0)
+            low = mid + 1;
+        else
+            high = mid;
+        mid = low + (high-low) / 2;}
+    if(mid < length){
+        int i = length;
+        for(;i>mid;i--)
+            list[i] = list[i-1];}
+    list[mid] = num;
+}
+
 int main(){
     freopen(INPUT, "r", stdin);
     freopen(OUTPUT, "w", stdout);
+    int m, s, c;
+    scanf("%d %d %d\n", &m, &s, &c);
+    int i, j = 0, k;
+    int stalls[c];
+    for(i=0;i<=c;i++){
+        k = j;
+        if(j > 0){
+            scanf("%d", &j);
+            printf("%d\n",j-k);
+            bisect(j-k, stalls, i);
+        }
+        else
+            scanf("%d", &j);
+    }
+//    for(i=0;i<c;i++){
+//        printf("%d\n",stalls[i]);
+//    }
 
-
-    exit(0)
+    exit(0);
 }
