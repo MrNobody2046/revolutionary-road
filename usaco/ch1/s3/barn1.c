36d9f5da71f4707cf3ce43e98943e71cb1dc8ec2
philoprove <sphy@foxmail.com>
2014-10-08 18:16:57 +0800

    ...

---
+++ usaco/ch1/s3/barn1.c
@@ -0,0 +1,21 @@
+/*
+ID:sphy1
+LANG: C
+TASK: barn1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT "barn1.in"
+#define OUTPUT "barn1.out"
+
+
+int main(){
+    freopen(INPUT, "r", stdin);
+    freopen(OUTPUT, "w", stdout);
+
+
+    exit(0)
+}
