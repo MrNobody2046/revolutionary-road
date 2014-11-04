5ab474d93e6b12608f3a6f3f261e7f24d193ae20
kenny <kenny@kenny@xmouse.vm.com>
2014-11-04 22:31:59 +0800

    ...

--- usaco/ch1/s4/clocks.c
+++ usaco/ch1/s4/clocks.c
@@ -13,17 +13,26 @@ TASK: clocks
 
 int rotations[9][9] = { {1,1,0,1,1,0,0,0,0},
                         {1,1,1,0,0,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0},
-                        {1,1,0,1,1,0,0,0,0} };
+                        {0,1,1,0,1,1,0,0,0},
+                        {1,0,0,1,0,0,1,0,0},
+                        {0,1,0,1,1,1,0,1,0},
+                        {0,0,1,0,0,1,0,0,1},
+                        {0,0,0,1,1,0,1,1,0},
+                        {0,0,0,0,0,0,1,1,1},
+                        {0,0,0,0,1,1,0,1,1} };
 
 //粗暴的方法是枚举，看着好像方程组
 //
 //
+
+void opt(int clocks[], int opts[])
+{
+    int i;
+    for(i=0;i<9;i++)
+    {
+        clocks[i] = (clocks[i] + opts[i]) % 4;
+    }
+}
 
 int main()
 {
@@ -33,11 +42,21 @@ int main()
     int clocks[9];
     for(i=0;i<3;i++)
     {
-        scanf("%d %d %d", &clocks[i], &clocks[i+1], &clocks[i+2]);
+        scanf("%d %d %d", &clocks[3*i], &clocks[3*i+1], &clocks[3*i+2]);
     }
-    for(i=0;i<3;i++)
+    for(i=0;i<9;i++)
     {
         clocks[i] = clocks[i] / 3;
     }
+    opt(clocks, rotations[3]);
+    opt(clocks, rotations[4]);
+    opt(clocks, rotations[7]);
+    opt(clocks, rotations[8]);
+    for(i=0;i<9;i++)
+    {
+        printf("%d\t", clocks[i]);
+    }
+
+
     exit(0);
 }
