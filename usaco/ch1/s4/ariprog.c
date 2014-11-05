a66465b38b78a041cbc67ad63d21d66c7ace8c03
kenny <kenny@kenny@xmouse.vm.com>
2014-11-05 23:28:26 +0800

    finished basic logic, but still not correct.

--- usaco/ch1/s4/ariprog.c
+++ usaco/ch1/s4/ariprog.c
@@ -28,7 +28,7 @@ void build_pow_list(int max)
 
 int print_array(int li[])
 {
-    int i, size= sizeof(li);
+    int i, size= 64;
     for(i=0;i<size;i++)
     {
         printf("%d ",li[i]);
@@ -43,7 +43,7 @@ void comb(int array[], int length, int picked)
         return;
     }
     if(picked<=0){
-        printf("%d\n",ret[0] + ret[1]);
+//        printf("%d\n",ret[0] + ret[1]);
         ALL[ret[0] + ret[1]] ++;
         return;
     }
@@ -58,14 +58,30 @@ void comb(int array[], int length, int picked)
     return;
 }
 
+int in_comb(int number){
+    if(ALL[number]==0)
+    {
+        return 0;
+    }
+    return 1;
+}
 
+int found(int start,int b,int needed)
+{
+    int i;
+    for(i=0;i<needed;i++){
+        if(in_comb(start+i*b)==0)
+        {
+            return 0;
+        }
+    }
+}
 
 int main(){
     freopen(INPUT, "r", stdin);
     freopen(OUTPUT, "w", stdout);
     int N, M;
     scanf("%d\n%d", &N, &M);
-    printf("%d\n%d", N, M);
     int maxcomb = M*M;
     build_pow_list(M);
     ALL = (int*)malloc(2*M*M*sizeof(int));
@@ -76,7 +92,18 @@ int main(){
     }
 //    print_array(ALL);
     comb(POW_TABLE,M+1,2); // list all a^2 + b^2
-    print_array(ALL);
-    printf("%d\n%d", (int)sizeof(ALL), ALL[64]);
+//    print_array(ALL);
+//    printf("%d\n%d", (int)sizeof(ALL), ALL[64]);
+    int max_b = M*M*2/N + 1;
+    int b,start;
+    for(start=0;start<maxcomb;start++){
+        for(b=0;b<=max_b;b++)
+        {
+            if(found(start,b,N)!=0)
+            {
+                printf("%d %d\n", start, b);
+            }
+        }
+    }
     exit(0);
 }
