5428fc898d5f9f28e81a57bd12d5c9ded022f532
kenny <kenny@kenny@xmouse.vm.com>
2014-11-09 00:40:04 +0800

    start prog milk3

---
+++ usaco/ch1/s4/milk3.c
@@ -0,0 +1,20 @@
+/*
+ID:sphy1
+LANG: C
+TASK: milk3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT "milk3.in"
+#define OUTPUT "milk3.out"
+
+
+int main(){
+    freopen(INPUT, "r", stdin);
+    freopen(OUTPUT, "w", stdout);
+
+    exit(0);
+}
