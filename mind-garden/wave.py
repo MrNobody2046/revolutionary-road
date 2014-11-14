16d40d2349f522e0719fa068ba575c0916a75c4a
philoprove <sphy@foxmail.com>
2014-11-14 22:41:34 +0800

    start castle

---
+++ mind-garden/wave.py
@@ -0,0 +1,10 @@
+
+
+class Heap(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
+
+
+    def insert(self, data):
---
+++ usaco/ch2/castle.c
@@ -0,0 +1,20 @@
+/*
+ID:sphy1
+LANG: C
+TASK: castle
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT "castle.in"
+#define OUTPUT "castle.out"
+
+
+int main(){
+    freopen(INPUT, "r", stdin);
+    freopen(OUTPUT, "w", stdout);
+
+    exit(0);
+}
