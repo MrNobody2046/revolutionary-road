8c82c14883fe2f82811b15e3e3adf5f8b7268047
philoprove <sphy@foxmail.com>
2014-11-19 22:16:27 +0800

    ac

--- usaco/ch2/holstein.c
+++ usaco/ch2/holstein.c
@@ -14,108 +14,86 @@
 int vitamin_num, feed_num, i, j;
 int demand[25] = { 0 };
 int choice[25] = { 0 };
-int ret[25] = { 0 };
-int temp_ret[25] = { 0 };
+int ret[26] = { 0 };
+int temp_ret[26] = { 0 };
 
 typedef struct Feed {
-	int conten[25];
+    int conten[25];
 } Feed;
 
 Feed feeds[15];
 int found = 0;
 int idx = 0;
-void pp() {
-	for (i = 0; i <= feed_num; i++) {
-		printf("%d \t", choice[i]);
-	}
-	printf("\n");
-}
-
-void pp2() {
-	int _contain[25] = { 0 };
-	for (j = 0; j < vitamin_num; j++) {
-		for (i = 0; i < feed_num; i++) {
-			_contain[j] += choice[i] * feeds[i].conten[j];
-		}
-	}
-	for (i = 0; i < vitamin_num; i++) {
-		printf("%d \t", _contain[i]);
-	}
-	printf("\n");
-}
 
 void cpy_ret() {
-	for (i = 0; i < 25; i++) {
-		ret[i] = temp_ret[i];
-	}
+    for (i = 0; i < 26; i++) {
+        ret[i] = temp_ret[i];
+        temp_ret[i] = 0;
+    }
 
 }
 
 void dfs() {
-
-	if (idx < 0) {
-		pp();
-
-		int sum = 0;
-		for (i = 0; i < feed_num; i++) {
-			if (choice[i]) {
-				sum += choice[i];
-				temp_ret[sum] = i + 1;
-			}
-		}
-		temp_ret[0] = sum;
-		if (temp_ret[0] < ret[0]) {
-			cpy_ret();
-		}
-
-		return;
-	}
-
-	choice[idx] = 0;
-	idx--;
-	dfs();
-	idx++;
-	choice[idx] = 1;
-	idx--;
-	dfs();
-	idx++;
-
-	return;
+    if (idx < 0) {
+        if (match()) {
+            int sum = 0;
+            for (i = 0; i < feed_num; i++) {
+                if (choice[i]) {
+                    sum += choice[i];
+                    temp_ret[sum] = i + 1;
+                }
+            }
+            temp_ret[0] = sum;
+            if (temp_ret[0] < ret[0]) {
+                cpy_ret();
+            }
+        }
+        return;
+    }
+    choice[idx] = 0;
+    idx--;
+    dfs();
+    idx++;
+    choice[idx] = 1;
+    idx--;
+    dfs();
+    idx++;
+    return;
 }
 
 int match() {
-	int _contain[25] = { 0 };
-	for (j = 0; j < vitamin_num; j++) {
-		for (i = 0; i < feed_num; i++) {
-			_contain[j] += choice[i] * feeds[i].conten[j];
-		}
-		if (_contain[j] < demand[j]) {
-			return 0;
-		}
-	}
-	return 1;
+    int _contain[25] = { 0 };
+    for (j = 0; j < vitamin_num; j++) {
+        for (i = 0; i < feed_num; i++) {
+            _contain[j] += choice[i] * feeds[i].conten[j];
+        }
+        if (_contain[j] < demand[j]) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main() {
-	freopen(INPUT, "r", stdin);
-//	freopen(OUTPUT, "w", stdout);
-	scanf("%d", &vitamin_num);
-	ret[0] = 25;
-	for (i = 0; i < vitamin_num; i++) {
-		scanf("%d", &demand[i]);
-	};
-	scanf("%d", &feed_num);
-	for (i = 0; i < feed_num; i++) {
-		for (j = 0; j < vitamin_num; j++) {
-			scanf("%d", &(feeds[i].conten[j]));
-		};
-	};
-	idx = feed_num;
-	dfs();
+    freopen(INPUT, "r", stdin);
+    freopen(OUTPUT, "w", stdout);
+    scanf("%d", &vitamin_num);
+    ret[0] = 25;
+    for (i = 0; i < vitamin_num; i++) {
+        scanf("%d", &demand[i]);
+    };
+    scanf("%d", &feed_num);
+    for (i = 0; i < feed_num; i++) {
+        for (j = 0; j < vitamin_num; j++) {
+            scanf("%d", &(feeds[i].conten[j]));
+        };
+    };
+    idx = feed_num;
+    dfs();
 
-	for (i = 0; i < ret[0]; i++) {
-		printf("%d ", ret[i]);
-	}
-	printf("%d\n", ret[i]);
-//	exit(0);
+    for (i = 0; i < ret[0]; i++) {
+        printf("%d ", ret[i]);
+    }
+    printf("%d\n", ret[i]);
+    exit(0);
 }
