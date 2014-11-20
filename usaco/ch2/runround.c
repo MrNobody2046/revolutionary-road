459eb42f41c13674fe5b17481d2d72e7aeb029f6
philoprove <sphy@foxmail.com>
2014-11-21 00:18:10 +0800

    ...

--- usaco/ch2/runround.c
+++ usaco/ch2/runround.c
@@ -1,8 +1,8 @@
 /*
-ID:sphy1
-LANG: C
-TASK: runround
-*/
+ ID:sphy1
+ LANG: C
+ TASK: runround
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,34 +11,45 @@ TASK: runround
 #define INPUT "runround.in"
 #define OUTPUT "runround.out"
 
-
-int is_round_number(unsigned long a){
+int is_round_number(unsigned long a) {
     char temp[11];
     sprintf(temp, "%lu", a);
     int length = strlen(temp);
-    int i, current_number ,current_offset;
+    int i, current_number, current_offset;
+    current_number = -1;
     current_offset = 0;
-    for(i=0;i<=length;i++){
-        if(temp[current_offset] != '*' && (current_number = (temp[current_offset] - '0')) == length){
+    int all[10] = { 0 };
+    for (i = 0; i < length; i++) {
+        if ((temp[i] - '0') != 0 && all[temp[i] - '0'] == 0) {
+            all[temp[i] - '0']++;
+        } else {
             return 0;
         }
+    }
+    for (i = 0; i < length; i++) {
+        if (temp[current_offset] == '*'
+                || ((temp[current_offset] - '0') % length) == 0) {
+            return 0;
+        }
+        current_number = temp[current_offset] - '0';
         temp[current_offset] = '*';
         current_offset = (current_offset + current_number) % length;
-//        printf("c:%d o:%d ",current_number, current_offset);
-//        printf("%s\n", temp);
+    }
+    if (current_offset != 0) {
+        return 0;
     }
     return 1;
 
 }
 
-int main(){
+int main() {
     freopen(INPUT, "r", stdin);
-//    freopen(OUTPUT, "w", stdout);
-    unsigned long  n;
+    freopen(OUTPUT, "w", stdout);
+    unsigned long n;
     scanf("%lu", &n);
     unsigned long s = ++n;
-    while(!is_round_number(s)){
-        s ++;
+    while (!is_round_number(s)) {
+        s++;
     }
     printf("%lu\n", s);
     exit(0);
