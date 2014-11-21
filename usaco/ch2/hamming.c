6b1dad6b1a6109f95c8c49eee90b969b502481f5
kenny <kenny@kenny@xmouse.vm.com>
2014-11-21 20:25:43 +0400

    ..

---
+++ usaco/ch2/hamming.c
@@ -0,0 +1,20 @@
+/*
+ID:sphy1
+LANG: C
+TASK: hamming
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT "hamming.in"
+#define OUTPUT "hamming.out"
+
+
+int main(){
+    freopen(INPUT, "r", stdin);
+    freopen(OUTPUT, "w", stdout);
+
+    exit(0);
+}
